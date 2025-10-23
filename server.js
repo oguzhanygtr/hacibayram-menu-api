c53f40d8d8f51652207b13c5cd4fee1ad694cf54
Oğuz Y <oguzhanygtr@outlook.com>
2025-10-23 20:14:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ export default async function handler(req, res) {
   try {
     const url = "https://yemek.hacibayram.edu.tr/";
 
-    // SSL doğrulamasını kapatan agent
-    const agent = new https.Agent({  
-      rejectUnauthorized: false  
-    });
+    // SSL hatası için
+    const agent = new https.Agent({ rejectUnauthorized: false });
 
     const { data } = await axios.get(url, { httpsAgent: agent });
-
     const $ = cheerio.load(data);
 
-    const gun = $("h4").first().text().trim();
-    const yemekler = [];
+    // Gün tarihi
+    const gun = $(".calendar-active").text().trim(); // aktif gün div'i
 
-    $("li").each((i, el) => {
+    // Yemek listesi
+    const yemekler = [];
+    $("#list li").each((i, el) => {
       yemekler.push($(el).text().trim());
     });
 
+    // XML oluştur
     const xml = `
 <menu>
   <gun>${gun}</gun>
@@ -32,6 +32,7 @@ export default async function handler(req, res) {
 
     res.setHeader("Content-Type", "application/xml");
     res.status(200).send(xml);
+
   } catch (error) {
     res.status(500).send(`<error>${error.message}</error>`);
   }
