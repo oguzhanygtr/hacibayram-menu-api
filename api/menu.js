ca203bba635b04684fbea08e9f15c84be968384c
Oğuz Y <oguzhanygtr@outlook.com>
2025-10-23 21:45:22 +0300

    Update menu.js

--- api/menu.js
+++ api/menu.js
@@ -1,27 +1,26 @@
 import fetch from "node-fetch";
+import https from "https";
 
 export default async function handler(req, res) {
   try {
-    // JSON verisini çek
-    const response = await fetch("https://yemek.hacibayram.edu.tr/load-menu");
+    const agent = new https.Agent({
+      rejectUnauthorized: false // SSL sertifika hatalarını yok say
+    });
+
+    // JSON endpoint
+    const response = await fetch("https://yemek.hacibayram.edu.tr/load-menu", { agent });
     const data = await response.json();
 
-    // JSON'dan XML formatına dönüştür
+    // XML dönüşümü
     const xml = `
 <menu>
   <gun>${data.gun}</gun>
   <yemekler>
-    ${data.yemekler.map(y => `
-      <yemek>
-        <ad>${y.ad}</ad>
-        <kalori>${y.kalori}</kalori>
-      </yemek>
-    `).join("\n    ")}
+    ${data.yemekler.map(y => `<yemek>${y.ad} (${y.kalori} Kcal)</yemek>`).join("\n    ")}
   </yemekler>
 </menu>
 `.trim();
 
-    // XML'i döndür
     res.setHeader("Content-Type", "application/xml; charset=utf-8");
     res.status(200).send(xml);
   } catch (err) {
